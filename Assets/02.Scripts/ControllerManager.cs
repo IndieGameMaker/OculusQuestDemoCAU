245bb69f18a339a396d57f15de91e3cb3792a630
Lee JaeHyun <myleje@gmail.com>
2021-01-19 17:24:22 +0900

    first commit

---
+++ Assets/02.Scripts/ControllerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+    public OVRInput.Controller leftController = OVRInput.Controller.LTouch;
+    public OVRInput.Controller rightController = OVRInput.Controller.RTouch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, leftController))
+        {
+            Debug.Log("왼손 Index Trigger !!");
+        }
+        if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger, rightController))
+        {
+            Debug.Log("오른손 Index Trigger !!");
+        }
+    }
+}
