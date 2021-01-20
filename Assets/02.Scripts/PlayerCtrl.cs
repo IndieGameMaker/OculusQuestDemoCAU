60e7d469a166cddd2f586317d4bc20719a704812
Lee JaeHyun <myleje@gmail.com>
2021-01-20 14:58:46 +0900

    .

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public Image indexPressed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Touch.SecondaryIndexTrigger))
+        {
+            float rate = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger); //0.0f ~ 1.0f
+            indexPressed.fillAmount = rate;
+        }        
+    }
+}
